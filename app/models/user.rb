3f33773d4ff02bed93f3d3c603ee73d9f7f0a7ef
THitokuse <hasegawa.taichiro@gmail.com>
2019-04-24 02:14:28 +0900

    userテーブルとgroupテーブルにアソシエーション付与

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:email]
 
   attr_accessor :group_key
+
+  #association
+  belongs_to :group
 end
