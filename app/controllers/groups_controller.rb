034df66dc6319303d64fc67c6a91c0fb3d3b1fed
THitokuse <hasegawa.taichiro@gmail.com>
2019-04-26 02:22:47 +0900

    グループページを編集できるように実装

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,22 @@
 class GroupsController < ApplicationController
+    before_action :group_find, only: [:show, :edit, :update]
+
     def show
+    end
+
+    def edit
+    end
+
+    def update
+        @group.update(update_params)
+    end
+
+    private
+    def group_find
         @group = Group.find(params[:id])
+    end
+
+    def update_params
+        params.require(:group).permit(:key, :detail)
     end
 end
